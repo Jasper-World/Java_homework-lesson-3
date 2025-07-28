0bf078fc0a99d53d20f27e923581eede11fae6cd
Jasper-World <104810793+Jasper-World@users.noreply.github.com>
2025-07-28 13:32:26 -0400

    Add files via upload

---
+++ Polygon.java
@@ -0,0 +1,24 @@
+public class Polygon {
+}
+
+class Rectangle extends Polygon {
+}
+
+class Square extends Rectangle {
+}
+
+class Trapezium extends Rectangle {
+}
+
+class Triangle extends Polygon {
+}
+
+class obtuseTriangle extends Triangle {
+}
+
+class acuteTriangle extends Triangle {
+}
+
+class rightTriangle extends Triangle {
+
+}
